027a5beeeb62852c2e765675dd16ae265037eeef
emanuelly-martins <emanuelly.ramos@edu.senai.br>
2026-09-10 11:30:58 -0300

    acessando propriedades

---
+++ nivel01/ex02.js
@@ -0,0 +1,12 @@
+// 🟢 NÍVEL 1 — COMEÇANDO
+// 2. Acessando propriedades
+
+const produto = {
+    nome: 'Teclado',
+    preco: 150,
+    marca: 'Logitech',
+};
+
+console.log(produto.nome);
+console.log(produto.preco);
+console.log(produto.marca);
