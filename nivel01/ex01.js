be718c3abb1c51f6a4a931a8eb23779be9b16dd1
emanuelly-martins <emanuelly.ramos@edu.senai.br>
2026-09-10 11:27:57 -0300

    criando um objeto

---
+++ nivel01/ex01.js
@@ -0,0 +1,12 @@
+// 🟢 NÍVEL 1 — COMEÇANDO
+// 1. Criando um objeto
+
+const aluno = {
+    nome: 'Emanuelly',
+    idade: 16,
+    curso: 'Desenvolvimento de Sistemas',
+};
+
+console.log(`Nome: ${aluno.nome}`);
+console.log(`Idade: ${aluno.idade}`);
+console.log(`Curso: ${aluno.curso}`);
