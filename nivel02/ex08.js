f4c2bdf9af6650ef137a99465663847f7e169f5e
emanuelly-martins <emanuelly.ramos@edu.senai.br>
2026-09-10 11:53:56 -0300

    primeira desestruturação

---
+++ nivel02/ex08.js
@@ -0,0 +1,12 @@
+// 🟡 NÍVEL 2 — DESESTRUTURAÇÃO
+// 8. Primeira desestruturação
+
+const aluno = {
+    nome: 'Maria',
+    idade: 17,
+    turma: '2TDS',
+};
+
+console.log(`Nome: ${aluno.nome}`);
+console.log(`Idade: ${aluno.idade}`);
+console.log(`Curso: ${aluno.turma}`);
