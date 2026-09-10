41e7ded1c09abdede230e46d97fb061d8d56f51c
emanuelly-martins <emanuelly.ramos@edu.senai.br>
2026-09-10 11:47:08 -0300

    criando uma mensagem

---
+++ nivel01/ex07.js
@@ -0,0 +1,9 @@
+// 🟢 NÍVEL 1 — COMEÇANDO
+// 7. Criando uma mensagem
+
+const apresentar = {
+    nome: "Emanuelly",
+    curso: "Desenvolvimento de sistemas"
+};
+
+console.log(`Olá, meu nome é ${apresentar.nome} e estudo ${apresentar.curso}.`);
