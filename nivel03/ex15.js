e0b1628ce9a22ae7d0ea8b23b398b1e2d28e238e
emanuelly-martins <emanuelly.ramos@edu.senai.br>
2026-09-17 14:17:24 -0300

    calculando desconto

---
+++ nivel03/ex15.js
@@ -0,0 +1,7 @@
+// 🔴 NÍVEL 3 — JUNTANDO TUDO
+// 15. Calculando desconto
+
+const calcularDesconto = (preco, desconto) => {
+    return preco - desconto;
+};
+console.log(calcularDesconto(200, 20));
