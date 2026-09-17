1d15f31946987d528958f5411c3701319ef460a3
emanuelly-martins <emanuelly.ramos@edu.senai.br>
2026-09-17 13:23:45 -0300

    pegando apenas algumas propriedades

---
+++ nivel02/ex09.js
@@ -0,0 +1,11 @@
+// 🟡 NÍVEL 2 — DESESTRUTURAÇÃO
+// 9. Pegando apenas algumas propriedades
+
+const usuario = {
+    nome: 'Pedro',
+    idade: 20,
+    email: 'pedro@email.com',
+    cidade: 'São Paulo',
+};
+
+console.log(`${usuario.nome} - ${usuario.email}`);
