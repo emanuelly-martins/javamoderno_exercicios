3e9f045b4f213f842dee30d442292bd58b8e4ef1
emanuelly-martins <emanuelly.ramos@edu.senai.br>
2026-09-17 14:26:46 -0300

    média de um aluno

---
+++ nivel03/ex17.js
@@ -0,0 +1,14 @@
+// 🔴 NÍVEL 3 — JUNTANDO TUDO
+// 17. Média de um aluno
+
+const aluno = {
+    nome: 'Lucas',
+    nota1: 8,
+    nota2: 6,
+};
+
+const calcularMedia = ({ nome, nota1, nota2 }) => {
+    return (nota1 + nota2) / 2;
+}
+
+console.log(`${aluno.nome} ficou com média ${calcularMedia(aluno)}`);
