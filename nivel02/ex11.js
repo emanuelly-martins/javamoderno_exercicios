ec179242e654e63bd342cf209a45cbf74f2e2a37
emanuelly-martins <emanuelly.ramos@edu.senai.br>
2026-09-17 13:40:13 -0300

    informações de um carro

---
+++ nivel02/ex11.js
@@ -0,0 +1,12 @@
+// 🟡 NÍVEL 2 — DESESTRUTURAÇÃO
+// 11. Informações de um carro
+
+const carro = {
+    marca: 'Toyota',
+    modelo: 'Corolla',
+    ano: 2024,
+};
+
+const { marca, modelo, ano } = carro;
+
+console.log(`${marca} ${modelo} - Ano ${ano}`);
